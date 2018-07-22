e7aacba031aa8cf88031f5a9c188ed428be970cf
Karan Agarwal <091karan@gmail.com>
2018-07-22 10:22:24 +0530

    Initial commit

---
+++ thumbnail/models.py
@@ -0,0 +1,17 @@
+import os
+from django.db import models
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PATH = os.path.join(BASE_DIR,'thumbnail')
+
+class Thumb(models.Model):
+    type = models.TextField()
+    media = models.FileField()
+    thumbnailcreated = models.ImageField()
+
+    def __str__(self):
+        return '{}'.format(self.type)
+
+    def delete(self, *args, **kwargs):
+        os.remove(PATH + "/" + self.media.name)
+        super(Thumb,self).delete(*args,**kwargs)
---
+++ thumbnail/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Thumb
+import cv2
+import numpy as np
+import os
+from PIL import Image
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PATH = os.path.join(BASE_DIR,'thumbnail/media')
+
+def home(request):
+    if request.method=="POST":
+        thumb = Thumb()
+        thumb.media = request.FILES['file']
+        thumb.type = request.POST['filetype']
+        thumb.save()
+        if request.POST['filetype']=="video":
+            vidcap = cv2.VideoCapture(PATH + '/12.mp4')
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,2000)
+            success,image = vidcap.read()
+            count = 0
+            while success:
+                cv2.imwrite(PATH + "/frame%d.jpg" % count, image)     # save frame as JPEG file
+                success,image = vidcap.read()
+                print('Read a new frame: ', success)
+                count += 1
+                break
+
+            images = np.array(Image.open(PATH + "/frame0.jpg"))
+
+        elif request.POST['filetype']=="image":
+            images = np.array(Image.open(PATH + "/" + request.FILES['file'].name))
+
+        elif request.POST['filetype']=="audio":
+            images = np.array(Image.open(PATH + "/audio.jpg"))
+
+        else:
+            images = np.array(Image.open(PATH + "/text.jpg"))
+
+        resized_image = cv2.resize(images, (200, 200), interpolation = cv2.INTER_AREA)
+        img = Image.fromarray(resized_image)
+        img.show()
+        return redirect('created')
+
+        t = Thumb.objects.all()
+        for t_obj in t:
+            t_obj.delete()
+
+
+    else:
+        return render(request,'thumbnail/home.html')
+
+def created(request):
+    return render(request,'thumbnail/created.html')
